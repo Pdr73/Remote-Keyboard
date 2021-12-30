01922593d740a4dc5f20761772a8253f16194365
Pedro Vico <pedrovicocantero@hotmail.com>
2021-12-30 19:24:31 +0100

    Add files via upload

--- tcpclnt.py
+++ tcpclnt.py
@@ -1,23 +1,41 @@
-# CLIENTE
+# CLIENT
 import socket, msvcrt, sys
+from pynput.keyboard import Key, Controller
+
+# Dictionary with the keys
+dicsionario = {"Key.enter":Key.enter, "Key.delete":Key.delete, "Key.insert":Key.insert, "Key.right":Key.right, "Key.down":Key.down, "Key.left":Key.left, "Key.ctrl_r":Key.ctrl_r, "Key.cmd":Key.cmd, "Key.ctrl_l":Key.ctrl_l, "Key.alt_gr":Key.alt_gr, "<":"<", "Key.space":Key.space, "Key.alt_l":Key.alt_l, "Key.ctrl_l":Key.ctrl_l, "Key.page_down":Key.page_down, "Key.down":Key.down, "Key.end":Key.end, "Key.up":Key.up, "Key.shift_r":Key.shift_r, "-":"-", ".":".", ",":",", "m":"m", "n":"n", "b":"b", "v":"v", "c":"c", "x":"x", "z":"z", "Key.shift":Key.shift, "+":"+", "Key.right":Key.right, "Key.left":Key.left, "Key.enter":Key.enter, "[u'\xb4']":u'\xb4', "u'\xf1'":u'\xf1', "l":"l", "k":"k", "j":"j", "h":"h", "g":"g", "f":"f", "d":"d", "s":"s", "a":"a", "Key.caps_lock":Key.caps_lock, "Key.page_up":Key.page_up, "Key.up":Key.up, "Key.home":Key.home, "u'\xe7'":u'\xe7', "+":"+", "`":"`", "p":"p", "o":"o", "i":"i", "u":"u", "y":"y", "t":"t", "r":"r", "e":"e", "w":"w", "q":"q", "Key.tab":Key.tab, "-":"-", "*":"*", "-":"-", "Key.num_lock":Key.num_lock, "Key.backspace":Key.backspace, "u'\xa1'":u'\xa1', "'":"'", "0":"0", "9":"9", "8":"8", "7":"7", "6":"6", "5":"5", "4":"4", "3":"3", "2":"2", "1":"1", "u'\xba'":u'\xba', "Key.page_down":Key.page_down, "Key.page_up":Key.page_up, "Key.delete":Key.delete, "Key.insert":Key.insert, "Key.pause":Key.pause, "Key.scroll_lock":Key.scroll_lock, "Key.print_screen":Key.print_screen, "Key.f12":Key.f12, "Key.f11":Key.f11, "Key.f10":Key.f10, "Key.f9":Key.f9, "Key.f8":Key.f8, "Key.f7":Key.f7, "Key.f6":Key.f6, "Key.f5":Key.f5, "Key.f4":Key.f4, "Key.f3":Key.f3, "Key.f2":Key.f2, "Key.f1":Key.f1, "Key.esc":Key.esc}
+dicsionariodos = dict(map(reversed, dicsionario.items()))  	# inverted dicsionario to access the str from value
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 50000))
 
-exit = 0
+char = msvcrt.getch().encode('utf-8')
+ip = str(raw_input("IP: "))
+if ip == '':
+	ip = 'localhost'
 
-while exit == 0:
-	print '\r',
-	print '\\> ',
-	char = msvcrt.getch().encode('utf-8')	# Texto a enviar codificado
-	s.send(char)							# Lo envia
-	print char,
-	sys.stdout.flush()
-	# Key.backspace	= '\x08'
-	# Key.esc		= '\x1b'
+s.connect((ip, 50000))
 
-	if str(char) == '\x1b':
+def on_press(key):
+	try:
+		if key != keyboard.Key.esc:
+			s.send("press" + dicsionariodos[key]) 	# sends str, i.e.: "pressKey.delete" if it is no the esc key
+	except:
+		pass
+
+def on_release(key):
+	try:
+		s.send("release" + dicsionariodos[key])  	# sends str, i.e.: "releaseKey.delete" if it is no the esc key
+	except:
+		pass
+
+	if key==keyboard.Key.esc:
+		# Stops listener
 		s.send('/e')
-		exit = 1
+		s.close()
+		return False
 
-s.close()
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+	listener.join()
+
+listener=keyboard.Listener(on_press=on_press,on_release=on_release)
+listener.start()
--- tcpserv.py
+++ tcpserv.py
@@ -1,52 +1,39 @@
-# SERVIDOR
-import socket, sys
+# SERVER
+import socket, msvcrt
 from pynput.keyboard import Key, Controller
 
-keyboard = Controller()
+dicsionario = {"Key.enter":Key.enter, "Key.delete":Key.delete, "Key.insert":Key.insert, "Key.right":Key.right, "Key.down":Key.down, "Key.left":Key.left, "Key.ctrl_r":Key.ctrl_r, "Key.cmd":Key.cmd, "Key.ctrl_l":Key.ctrl_l, "Key.alt_gr":Key.alt_gr, "<":"<", "Key.space":Key.space, "Key.alt_l":Key.alt_l, "Key.ctrl_l":Key.ctrl_l, "Key.page_down":Key.page_down, "Key.down":Key.down, "Key.end":Key.end, "Key.up":Key.up, "Key.shift_r":Key.shift_r, "-":"-", ".":".", ",":",", "m":"m", "n":"n", "b":"b", "v":"v", "c":"c", "x":"x", "z":"z", "Key.shift":Key.shift, "+":"+", "Key.right":Key.right, "Key.left":Key.left, "Key.enter":Key.enter, "[u'\xb4']":u'\xb4', "u'\xf1'":u'\xf1', "l":"l", "k":"k", "j":"j", "h":"h", "g":"g", "f":"f", "d":"d", "s":"s", "a":"a", "Key.caps_lock":Key.caps_lock, "Key.page_up":Key.page_up, "Key.up":Key.up, "Key.home":Key.home, "u'\xe7'":u'\xe7', "+":"+", "`":"`", "p":"p", "o":"o", "i":"i", "u":"u", "y":"y", "t":"t", "r":"r", "e":"e", "w":"w", "q":"q", "Key.tab":Key.tab, "-":"-", "*":"*", "-":"-", "Key.num_lock":Key.num_lock, "Key.backspace":Key.backspace, "u'\xa1'":u'\xa1', "'":"'", "0":"0", "9":"9", "8":"8", "7":"7", "6":"6", "5":"5", "4":"4", "3":"3", "2":"2", "1":"1", "u'\xba'":u'\xba', "Key.page_down":Key.page_down, "Key.page_up":Key.page_up, "Key.delete":Key.delete, "Key.insert":Key.insert, "Key.pause":Key.pause, "Key.scroll_lock":Key.scroll_lock, "Key.print_screen":Key.print_screen, "Key.f12":Key.f12, "Key.f11":Key.f11, "Key.f10":Key.f10, "Key.f9":Key.f9, "Key.f8":Key.f8, "Key.f7":Key.f7, "Key.f6":Key.f6, "Key.f5":Key.f5, "Key.f4":Key.f4, "Key.f3":Key.f3, "Key.f2":Key.f2, "Key.f1":Key.f1, "Key.esc":Key.esc}
 
-def say(key):
-	keyboard.press(key)
-	keyboard.release(key)
+keybd = Controller()
 
+pcname = socket.gethostname()
+ip = socket.gethostbyname(pcname)
+
+print("This is the IP you have to share: {}?".format(ip))
+
+print("[*] Creating connection...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 50000))
+s.bind((ip, 50000))
 s.listen(1)
 conn, addr = s.accept()
+print("[+] Connected to {}! Program is ready to send keys!".format(addr))
 
 exit = 0
 tdata=[]
 
 while exit == 0:
-	data = conn.recv(1024)		# Recibimos datos
-	data = data.decode('utf-8')	# Decodificamos
+	data = conn.recv(1024)		# Recieve the data
+	data = data.decode('ascii')	# Decode
 
-	# Falta que hagas que presione las teclas
+	if data.startswith('press'):
+		keybd.press(dicsionario[data.split('press')[1]])
 
-	if str(data) == '\x08':		# Borrar una letra
-		tdata = tdata[:-1]
-		print '\r',
-		print "[+] " + "".join(tdata),
-		sys.stdout.flush()
-		key = Key.backspace
-	
-	elif str(data) == '\r':		# Intro
-		tdata.append('\n')
-		key = Key.space
-	
-	else:						# Simplemente añade una letra 
-		key = tdata				# La letra en cuestion
-		tdata.append(data)
-
-	print '\r',
-	print "[+] " + "".join(tdata),
-
-	say(key)
-
-	sys.stdout.flush()
+	elif data.startswith('release'):
+		keybd.release(dicsionario[data.split('release')[1]])
 
 	if data == '/e':
 		exit = 1
-		
+
 conn.close()
 s.close()
 print("\n[-] Connection ended.")
